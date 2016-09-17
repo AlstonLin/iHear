65217d0baa675fbcec42cd31e67cdf5b18255a96
Alston Lin <alstonlin812@gmail.com>
2016-09-17 06:01:55 -0400

    Changed port number to 80

--- Server/app.js
+++ Server/app.js
@@ -1,10 +1,10 @@
 var express = require("express");
 var app = express();
-
+var PORT = 80;
 app.get("/", function(req, res){
   res.send("Hello World!");
 });
 
-app.listen(8080, function(){
-  console.log("Listening on port 8080");
+app.listen(PORT, function(){
+  console.log("Listening on port " + PORT);
 });
